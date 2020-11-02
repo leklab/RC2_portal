566c051f1c1585e6d464cfe70afc9da35424e3df
monkollek <monkol.lek@yale.edu>
2020-11-02 18:33:39 +0000

    Clean up of ExpressionPage code

--- web_portal/src/client/ExpressionPage/ExpressionPage.js
+++ web_portal/src/client/ExpressionPage/ExpressionPage.js
@@ -63,101 +63,43 @@ const GENOTYPE_COLORS = {
   WT: 'rgba(44, 160, 101, 0.5)'  
 }
 
+
+const base_y_axis = {
+  autorange: true,
+  zeroline: true,
+  gridcolor: 'rgb(255, 255, 255)',
+  gridwidth: 1,
+  zerolinecolor: 'rgb(255, 255, 255)',
+  zerolinewidth: 2,
+  exponentformat: 'E',
+  title: 'Raw Read Count',
+  rangemode: 'tozero'  
+}
+
+const base_layout = {
+  //title: `${variantId} Expression`,
+  autosize: false,
+  width: 800,
+  height: 500,
+  margin: {
+    l: 60,
+    r: 30,
+    b: 60,
+    t: 80
+  },
+  paper_bgcolor: 'rgb(243, 243, 243)',
+  plot_bgcolor: 'rgb(243, 243, 243)',
+  showlegend: false,
+  boxmode: 'group',
+  xaxis: {
+    title: 'Timepoint'
+  }
+}
+
+
 //const ExpressionPage = ({ variantId }) => {
 
 export class ExpressionPage extends Component {
-  /*
-  const expression_data = await fetchExpression(variantId)
-  console.log(expression_data)
-
-  //const layout = {width: 800, height: 400, title: 'N00b test'}
-
-  const layout = {
-    title: `${variantId} Expression`,
-    autosize: false,
-    width: 800,
-    height: 500,
-    yaxis: {
-      autorange: true,
-      zeroline: true,
-      gridcolor: 'rgb(255, 255, 255)',
-      gridwidth: 1,
-      zerolinecolor: 'rgb(255, 255, 255)',
-      zerolinewidth: 2
-    },
-    boxmode: 'group',
-    margin: {
-      l: 40,
-      r: 30,
-      b: 80,
-      t: 100
-    },
-    paper_bgcolor: 'rgb(243, 243, 243)',
-    plot_bgcolor: 'rgb(243, 243, 243)'
-
-  }
-
-  const trace5 = {
-    y: [26, 36, 56],
-    x: ['Week 7', 'Week 7', 'Week 7'],
-    type: 'box',
-    name: 'PKD1 KO',
-    boxpoints: 'all',
-    hoveron: 'points',
-    marker: { size: 8 }
-  }
-
-  const trace6 = {
-    y: [16, 17, 58],
-    x: ['Week 7', 'Week 7', 'Week 7'],
-    type: 'box',
-    name: 'DKO',
-    boxpoints: 'all',
-    hoveron: 'points',
-    marker: { size: 8 }
-  }
-
-  const trace7 = {
-    y: [22, 30, 20],
-    x: ['Week 7', 'Week 7', 'Week 7'],
-    type: 'box',
-    name: 'WT',
-    boxpoints: 'all',
-    hoveron: 'points',
-    marker: { size: 8 }
-  }
-
-  const data = [ trace5, trace6, trace7 ];
-
-
-  let plot_data = []
-  console.log(expression_data.length)
-
-  for (var i = 0; i < expression_data.length; i++){
-
-    let x_data = []
-    
-    for(var j= 0; j < expression_data[i].read_count.length; j++){
-      x_data.push(expression_data[i].time_point)
-    }
-
-    console.log(x_data)
-
-    
-    
-    plot_data.push({
-      y: expression_data[i].read_count,  
-      x: x_data,
-      name: expression_data[i].genotype,
-      boxpoints: 'all',
-      hoveron: 'points',
-      marker: {size: 8}
-    })
-    
-
-  }
-  */
-
 
   static propTypes = {
     variantId: PropTypes.string.isRequired,
@@ -216,7 +158,7 @@ export class ExpressionPage extends Component {
 
   onGenotypeFilter = (state) => {
     console.log("In Genotype Filter")
-    console.log(state)
+    //console.log(state)
     this.setState(state)
   }
 
@@ -241,43 +183,13 @@ export class ExpressionPage extends Component {
   	console.log("In componentDidUpdate")
 
     if (this.props.variantId !== prevProps.variantId){
-      console.log("props.variantId changed")
+      //console.log("props.variantId changed")
 
       const gene_data = await this.fetchExpression(this.props.variantId)
       this.setState({gene_data: gene_data})
     }
   	
   }
-
-
-  /* 
-  async loadData() {
-
-    const { variantId } = this.props
-
-    const expression_data = await fetchExpression(variantId)
-    console.log(expression_data)
-    console.log(expression_data.length)
-
-    this.setState({data: expression_data})
-
-  }
-  */
-
-  /*
-  componentDidUpdate(prevProps) {
-    if (
-      this.props.query !== prevProps.query ||
-      !areVariablesEqual(this.props.variables, prevProps.variables)
-    ) {
-      this.loadData()
-    }
-  }
-
-  componentWillUnmount() {
-    this.mounted = false
-  }
-  */
 
   includeDataPoint = (data) => {
 
@@ -298,63 +210,10 @@ export class ExpressionPage extends Component {
     
   }
 
-  render() {
-
-    const { variantId } = this.props
-    const {gene_data} = this.state
-
-    console.log("Rendering ExpressionPage")
-
-    if(!this.mounted){
-      console.log("ExpressionPage not mounted")
-      return(
-        <Page>
-        <DocumentTitle title="Loading" />
-        <PageHeading>Loading Data</PageHeading>
-        <p>Loading</p>
-      </Page>
-      )
-    }
-
-    //console.log(expression_data)
-
-    const layout = {
-      title: `${variantId} Expression`,
-      autosize: false,
-      width: 800,
-      height: 500,
-      yaxis: {
-        autorange: true,
-        zeroline: true,
-        gridcolor: 'rgb(255, 255, 255)',
-        gridwidth: 1,
-        zerolinecolor: 'rgb(255, 255, 255)',
-        zerolinewidth: 2,
-        title: 'Raw Read Count'
-      },
-      xaxis: {
-        title: 'Timepoint'
-      },
-      boxmode: 'group',
-      margin: {
-        l: 60,
-        r: 30,
-        b: 60,
-        t: 80
-      },
-      paper_bgcolor: 'rgb(243, 243, 243)',
-      plot_bgcolor: 'rgb(243, 243, 243)',
-      showlegend: false
-    }
-
-    const config = {
-      displayModeBar: false
-    }
-
+  filterData = (expression_data) => {
     let plot_data = []
     
-    //console.log(expression_data.length)
-    const expression_data = gene_data.gene.expression
+    //const expression_data = gene_data.gene.expression
     
     for (var i = 0; i < expression_data.length; i++){
 
@@ -386,13 +245,50 @@ export class ExpressionPage extends Component {
           color: GENOTYPE_COLORS[expression_data[i].genotype],
           size: 8
         }
-      })
-      
+      }) 
     }
-    
-    //console.log("PKD1_KO: " + this.state.includePKD1_KO)
-    //console.log(plot_data)
 
+    return plot_data
+
+  }
+
+  render() {
+
+    const { variantId } = this.props
+    const { gene_data } = this.state
+    
+    console.log("Rendering ExpressionPage")
+
+    if(!this.mounted){
+      console.log("ExpressionPage not mounted")
+      return(
+        <Page>
+        <DocumentTitle title="Loading" />
+        <PageHeading>Loading Data</PageHeading>
+        <p>Loading</p>
+      </Page>
+      )
+    }
+
+    const layout = {
+      title: `${variantId} Expression`,      
+      ...base_layout,
+      yaxis: {
+        ...base_y_axis
+      }
+    }
+
+    //console.log(test_layout)
+
+
+    const config = {
+      displayModeBar: false
+    }
+
+    const plot_data = this.filterData(gene_data.gene.expression)
+    //console.log("Filtered Plot Data")
+    //console.log(plot_data)
+    
     return (
       <Page>
         <DocumentTitle title="Gene Expression" />
@@ -400,48 +296,15 @@ export class ExpressionPage extends Component {
           <GeneInfoColumnWrapper>
             <MouseGeneInfo gene={gene_data.gene} />
           </GeneInfoColumnWrapper>
+        
+
         <Plot
           data={plot_data}
           layout={layout}
           config={config}
         />
+
         {/*<ControlSection>
-          Include Genotype:
-          <Checkbox
-            checked={this.state.includePKD1_KO}
-            id="PKD1_KO"
-            label="PKD1_KO"
-            disabled={
-              this.state.includePKD1_KO && !this.state.includeDKO && !this.state.includeWT
-            }
-            onChange={includePKD1_KO => {
-              this.setState({ includePKD1_KO })
-            }}
-          />
-          <Checkbox
-            checked={this.state.includeDKO}
-            id="DKO"
-            label="DKO"
-            disabled={
-              this.state.includeDKO && !this.state.includePKD1_KO && !this.state.includeWT
-            }
-            onChange={includeDKO => {
-              this.setState({ includeDKO })
-            }}
-          />
-          <Checkbox
-            checked={this.state.includeWT}
-            id="WT"
-            label="WT"
-            disabled={
-              this.state.includeWT && !this.state.includePKD1_KO && !this.state.includeDKO
-            }
-            onChange={includeWT => {
-              this.setState({ includeWT })
-            }}
-          />
-        </ControlSection>
-        <ControlSection>
           Include Timepoint:
           <Checkbox
             id="W7"
